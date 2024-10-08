a2e8573dbcb66b0335bb3c7c2f328e3be2edebcc
alexb <alexander.basil@dcx.ufpb.br>
2024-10-08 20:52:34 -0300

    so adicionei os getter e setters

--- src/main/java/Modelos/Cliente.java
+++ src/main/java/Modelos/Cliente.java
@@ -2,4 +2,21 @@ package Modelos;
 
 public class Cliente {
     private String nome;
+    private String numero;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
 }
