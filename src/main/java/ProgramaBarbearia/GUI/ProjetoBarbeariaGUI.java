abd020665a366d2c587c7805dc2f8b1311962821
Gabriel Felix Gomes <g.felix2303@gmail.com>
2024-10-17 14:58:51 -0300

    Adicionando classe Teste e Barra de menu

--- src/main/java/ProgramaBarbearia/GUI/ProjetoBarbeariaGUI.java
+++ src/main/java/ProgramaBarbearia/GUI/ProjetoBarbeariaGUI.java
@@ -3,6 +3,7 @@ package ProgramaBarbearia.GUI;
 import ProgramaBarbearia.Controladores.SistemaBarbearia;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicMenuBarUI;
 import javax.swing.text.JTextComponent;
 import java.awt.*;
 import java.util.EnumSet;
@@ -11,15 +12,15 @@ public class ProjetoBarbeariaGUI {
     private JFrame janela = new JFrame("Barbearia");
     private SistemaBarbearia sistema;
 
-    public ProjetoBarbeariaGUI(){
+    public ProjetoBarbeariaGUI() {
         this.sistema = new SistemaBarbearia();
 
         createDialog();
-        //createMenuBar();
+        createMenuBar();
         //createLayout();
     }
 
-    public void createDialog(){
+    public void createDialog() {
         JLabel espaco1, espaco2;
         JTextField espaco3;
 
@@ -33,10 +34,10 @@ public class ProjetoBarbeariaGUI {
 
         espaco3 = new JTextField("");
 
-        janela.setSize(1200, 800);
+        janela.setSize(800, 600);
         janela.setResizable(false);
         janela.getContentPane().setBackground(Color.BLACK);
-        janela.getContentPane().setLayout(new GridLayout(3,3));
+        janela.getContentPane().setLayout(new GridLayout(3, 3));
         janela.getContentPane().add(espaco1);
         janela.getContentPane().add(espaco2);
         janela.getContentPane().add(espaco3, BorderLayout.LINE_END);
@@ -44,9 +45,101 @@ public class ProjetoBarbeariaGUI {
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    public void setVisible(boolean b){
+    public void setVisible(boolean b) {
         janela.setVisible(b);
     }
 
+    private void customizeMenuBar(JMenuBar menuBar) {
 
+        menuBar.setUI(new BasicMenuBarUI() {
+
+            @Override
+            public void paint(Graphics g, JComponent c) {
+                g.setColor(Color.black);
+                g.fillRect(0, 0, c.getWidth(), c.getHeight());
+            }
+
+        });
+
+        MenuElement[] menus = menuBar.getSubElements();
+
+        for (MenuElement menuElement : menus) {
+
+            JMenu menu = (JMenu) menuElement.getComponent();
+            changeComponentColors(menu);
+            menu.setOpaque(true);
+
+            MenuElement[] menuElements = menu.getSubElements();
+
+            for (MenuElement popupMenuElement : menuElements) {
+
+                JPopupMenu popupMenu = (JPopupMenu) popupMenuElement.getComponent();
+                popupMenu.setBorder(null);
+
+                MenuElement[] menuItens = popupMenuElement.getSubElements();
+
+                for (MenuElement menuItemElement : menuItens) {
+
+                    JMenuItem menuItem = (JMenuItem) menuItemElement.getComponent();
+                    changeComponentColors(menuItem);
+                    menuItem.setOpaque(true);
+
+                }
+            }
+        }
+    }
+
+    private void changeComponentColors(Component comp) {
+        comp.setBackground(Color.black);
+        comp.setForeground(Color.white);
+    }
+
+    public void createMenuBar() {
+        menuBar = new JMenuBar();
+
+        menuArquivo = new JMenu("Arquivo");
+        menuArquivoSair = new JMenuItem("Sair");
+        //menuArquivoSair.addActionListener(a -> btnSair_Click());
+        menuArquivo.add(menuArquivoSair);
+
+        menuCliente = new JMenu("Cliente");
+        menuCadastrarCliente = new JMenuItem("Cadastrar");
+        //menuCadastrarCliente.addActionListener(a -> cadastrarCliente());
+        menuBuscarCliente = new JMenuItem("Buscar");
+        //menuBuscarCliente.addActionListener(a -> PesquisaCliente());
+        //menuApagarCliente.addActionListener(a -> apagarCliente());
+
+        //menuCliente.add(menuCadastrarCliente);
+        //menuCliente.add(menuBuscarCliente);
+        //menuCliente.add(menuApagarCliente);
+
+        menuAgendar = new JMenu("Agendar");
+        menuAgendarMarcarHorario = new JMenuItem("Marcar Horário");
+        //menuAgendarMarcarHorario.addActionListener(a -> btnAgendar_Click());
+        menuAgendarDesmarcar = new JMenuItem("Desmarcar Horário");
+        menuAgendar.add(menuAgendarMarcarHorario);
+        menuAgendar.add(menuAgendarDesmarcar);
+
+        menuBar.add(menuArquivo);
+        menuBar.add(menuAgendar);
+
+        janela.setJMenuBar(menuBar);
+        customizeMenuBar(menuBar);
+    }
+
+    //==================================================================================================================
+    //=============COMPONENTES======================================================================================================
+    //==================================================================================================================
+    private JMenuBar menuBar;
+
+    private JMenu menuArquivo;
+    private JMenuItem menuArquivoSair;
+    private JMenu menuCliente;
+    private JMenuItem menuCadastrarCliente;
+    private JMenuItem menuBuscarCliente;
+    private JMenuItem menuApagarCliente;
+
+    private JMenu menuAgendar;
+    private JMenuItem menuAgendarMarcarHorario;
+    private JMenuItem menuAgendarDesmarcar;
 }
