f94e5962959be0410f86aeaa75187838d0499e2d
Gabriel Felix Gomes <g.felix2303@gmail.com>
2024-10-17 15:00:22 -0300

    Adicionando classe Teste e Barra de menu

--- src/main/java/ProgramaBarbearia/Controladores/Barbearia.java
+++ src/main/java/ProgramaBarbearia/Controladores/Barbearia.java
@@ -1,5 +1,12 @@
 package ProgramaBarbearia.Controladores;
 
+import ProgramaBarbearia.Modelos.Barbeiro;
+import ProgramaBarbearia.Modelos.TipoDeCorte;
+import ProgramaBarbearia.Modelos.horariodata;
+
 public interface Barbearia {
+
     boolean CadastrarCliente(String nome, String telefone);
+    void CadastrarBarbeario(String Nome, horariodata HorarioLivre, TipoDeCorte Especialidade);
+    Barbeiro PesquisarBarbeiro(TipoDeCorte especialidade);
 }
